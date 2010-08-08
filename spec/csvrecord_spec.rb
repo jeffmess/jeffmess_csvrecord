062c2175a99baa97f31a43701dc2d3e162915de9
Jeffrey van Aswegen <jeffmess@gmail.com>
2010-08-08 14:12:25 +0200

    adding infrastructure to begin testing.

---
+++ spec/csvrecord_spec.rb
@@ -0,0 +1,21 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "CsvRecord" do
+  
+  it "should be able to read csv files in other directories" do
+    pending
+  end
+  
+  it "should raise an error if the file is not a csv file" do
+    pending
+  end
+  
+  it "should be able to create a class determined by the name of the file" do
+    pending
+  end
+  
+  it "should create an array of objects" do
+    pending
+  end
+  
+end
